d20563a406835e81af4ddfe8e2d4157be559cc0f
ananyaseelam <60414577+ananyaseelam@users.noreply.github.com>
2020-07-07 17:42:50 -0400

    Update riskAnalysis.py

--- backend/riskAnalysis.py
+++ backend/riskAnalysis.py
@@ -5,5 +5,7 @@ from googleApi import returnCounty, returnPlaceType
 location = 'Outer Banks'
 x = returnCounty(location)
 y = returnPlaceType(location)
+pc = findPercentChange(x)
 
+print(pc)
 print(x + ' County ' + str(y))
