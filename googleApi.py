681a008ebde8a4f576b47f9467335f7b129141a9
ananyaseelam <60414577+ananyaseelam@users.noreply.github.com>
2020-07-07 16:00:25 -0400

    Add files via upload

--- googleApi.py
+++ googleApi.py
@@ -3,40 +3,41 @@ import pprint
 import time
 import requests
 from googleplaces import GooglePlaces, types, lang
+import json #// sort, search, etc 
 
-API_KEY = get_api_key()
+# the result is a Python dictionary:
+API_KEY = 
 
-google_places = GooglePlaces(API_KEY)
+def returnCounty(address):
+    gmaps = googlemaps.Client(key=API_KEY)
+    oneplaceblob = gmaps.find_place(address, 'textquery')
+    oneplaceID = ((oneplaceblob['candidates'])[0])['place_id']
+    oneplace = gmaps.place(str(oneplaceID))
+    countyString = (oneplace['result'])['address_components']
+    county = countySearch(countyString)
+    #print(json.dumps(oneplace, sort_keys=True, indent=4))
+    return county
 
-# You may prefer to use the text_search API, instead.
-query_result = google_places.nearby_search(
-        location='1016 Chessridge Way, Morrisville, NC, 27560',
-        radius=200, types=[types.TYPE_FOOD])
+def returnPlaceType(address):
+    gmaps = googlemaps.Client(key=API_KEY)
+    oneplaceblob = gmaps.find_place(address, 'textquery')
+    oneplaceID = ((oneplaceblob['candidates'])[0])['place_id']
+    oneplace = gmaps.place(str(oneplaceID))
+    placeType = (oneplace['result'])['types']
+    return placeType
 
-for place in query_result.places:
-    # Returned places from a query are place summaries.
-    print (place.name)
-    print(place.types)
-    place.get_details()
-    #print (place.details)
+def countySearch(searchString):
+    searchString = str(searchString)
+    county = searchString[:searchString.find(' County')]
+    wordList = county.split()
+    return str(wordList[-1])[1:]
 
-#Text Search Instead
+location = 'Outer Banks'
+x = returnCounty(location)
+y = returnPlaceType(location)
+
+print(x + ' ' + str(y))
 
 
-gmaps = googlemaps.Client(key=API_KEY)
-#https://www.indjango.com/google-api-to-get-lat-long-data/
-address = "1016 Chessridge Way, Morrisville, NC, 27560"
 
-api_response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(address, API_KEY))
-api_response_dict = api_response.json()
-#print(api_response_dict)
-if api_response_dict['status'] == 'OK':
-    latitude = api_response_dict['results'][0]['geometry']['location']['lat']
-    longitude = api_response_dict['results'][0]['geometry']['location']['lng']
-    print ('Latitude:', latitude)
-    print ('Longitude:', longitude)
 
-loc = str(latitude) + ',' + str(longitude)
-#print (loc)
-places_result = gmaps.places_nearby(location=loc, radius=200,open_now=False, type='grocery')
-#print (places_result)
