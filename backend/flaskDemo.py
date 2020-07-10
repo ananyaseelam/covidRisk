ca2dd787d0da1fa9d6ae1443d0c959142e785de1
ananyaseelam <60414577+ananyaseelam@users.noreply.github.com>
2020-07-09 22:16:31 -0400

    Update flaskDemo.py

--- backend/flaskDemo.py
+++ backend/flaskDemo.py
@@ -1,22 +1,31 @@
 from flask import Flask
 from covidApi import findPercentChange
 from googleApi import returnCounty, returnPlaceType, getPlaceID
-from businessApi import returnBusiness
-
+from businessApi import returnPoptimes
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/risk") #GET to render homepage
 def calculateRisk():
-    location = 'Subway at South Roxboro Street'
+    location = 'Statue of Liberty'
     x = returnCounty(getPlaceID(location))
     y = returnPlaceType(location)
     pc = findPercentChange(x)
-    b = returnBusiness('Tuesday', 19)
+    b = returnPoptimes('Tuesday', 19)
     risk = (pc*100)*0.5 + b*0.5
-    riskJson = {'risk': risk}
+    riskDict = {'risk': risk}
+    riskJson = json.dumps(riskDict)
     return riskJson
 
+@app.route("/location", methods = ['POST']) #GET to render homepage
+def addLocation():
+    location = {'my house': 'is cool'}
+    locationjson= json.dumps(location)
+    return locationjson
 
+    
+
+#, methods= 'POST' to submit location 
 if __name__ == '__main__':
     app.run(debug=True)
