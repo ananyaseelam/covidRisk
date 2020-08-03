22501e63c64d41da132ca58f0825e08ce62f1f68
namyakodali <namyakodali@gmail.com>
2020-08-03 17:51:52 -0500

    firstpage ui

--- frontend/pages/FirstPage.js
+++ frontend/pages/FirstPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 //import react in our code.
-import { StyleSheet, View, Button, TextInput, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Text, ScrollView, Image } from 'react-native';
 //import all the components we are going to use.
+import { Button, LinearGradient, linearGradientProps, Header, Icon } from 'react-native-elements';
 import LocationForm from '../LocationForm';
 
 export default class FirstPage extends Component {
@@ -14,7 +15,7 @@ export default class FirstPage extends Component {
   }
   static navigationOptions = {
     //Setting the header of the screen
-    title: 'Covid Risk App',
+    title: 'Covid Risk Apps',
   };
   showForm = () => {
     this.setState({showForm: true})
@@ -24,39 +25,81 @@ export default class FirstPage extends Component {
   }
     
   render(){
-      const { navigate } = this.props.navigation;
-      return (
-        <View style={styles.container}>
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+          <Image 
+            source={require('./transparent.png')} 
+            style={styles.logo}
+          />
           <Text>
-            Welcome to the Covid Risk App! 
+            {"\n"}
           </Text>
-          <Button
-          title="Calculate Your Risk Here"
-          //Button Title
+          <Text style={styles.titleText}>
+            {"CovidRisk"}
+          </Text>
+        <View style={styles.bottom}>
+        <Button
+          raised
+          title="Calculate Your Risk"
+          type = "outline"
+          titleStyle={{ color: 'black', fontFamily: 'System', fontWeight: "bold"}}
+          buttonStyle={{
+            backgroundColor: 'white',
+            borderColor: '#45B5E9',
+            borderWidth:5,
+            borderRadius: 30,
+            paddingVertical: 7,
+            paddingHorizontal:40
+          }}
+        //Button Title
           onPress={() =>
-            navigate('SecondPage')
-          }
-          //On click of the button we will send
-          //the data as a Json from here to the Second Screen using navigation prop
-        />
+          navigate('SecondPage')
+        }/>
         </View>
-  
-      );
-    }
+      <Text>
+      {"\n"}
+      </Text>
+      </View>
+
+    );
   }
+}
   
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      padding: 16,
-    },
-    input: {
-      width: 200,
-      height: 44,
-      padding: 10,
-      marginBottom: 10,
-      backgroundColor: '#DBDBD6',
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    //justifyContent: 'center',
+    backgroundColor: "#A5DEF9"
+    
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#DBDBD6',
+  },
+  baseText: {
+    fontFamily: 'System'
+  },
+  titleText: {
+    fontSize: 50,
+    fontFamily:"System",
+    color: "black",
+    fontWeight: "bold"
+  },
+  logo: {
+    width: 225,
+    height: 225,
+    marginTop:75
+
+    
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 10
+  }
+});
