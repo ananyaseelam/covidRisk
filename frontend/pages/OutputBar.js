7eac177736c8575fc0627c929411e8574520ce30
ananyaseelam <60414577+ananyaseelam@users.noreply.github.com>
2020-08-02 20:27:48 -0400

    Create OutputBar.js

---
+++ frontend/pages/OutputBar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Slider from '@react-native-community/slider'
+
+'use strict';
+ 
+var React = require('react');
+var Slider = require('react-native-slider');
+var {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text,
+} = require('react-native');
+ 
+var SliderExample = React.createClass({
+  getInitialState() {
+    return {
+      value: 0.2,
+    };
+  },
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+        <Slider
+          value={this.state.value}
+          onValueChange={(value) => this.setState({value})} />
+        <Text>Value: {this.state.value}</Text>
+      </View>
+    );
+  }
+});
+ 
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+});
+ 
+AppRegistry.registerComponent('SliderExample', () => SliderExample);
